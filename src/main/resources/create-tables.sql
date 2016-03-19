930936c4b914ba7b99a258449485d13db86336c9
kurtov <kurtov@unact.ru>
2016-03-19 22:20:13 +0300

    create tables

---
+++ src/main/resources/create-tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE order_items (
+    id         SERIAL PRIMARY KEY,
+    order_id   INT,                 --Должен быть FK на orders
+    goods_name VARCHAR(255),        --Должен быть FK на goods
+    value      INT
+);
+
+create table remains (
+    id         SERIAL PRIMARY KEY,
+    goods_name VARCHAR(255),        --Должен быть FK на goods
+    value      INT                  --Величина остатка
+);
